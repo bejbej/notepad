1ae2c36a41a0bcce6ae65418850458c21298cb96
James Honzatko <bejbej.uit@gmail.com>
2017-01-24 23:36:15 -0600

    Cleaned up some styles
    - Removed some unused styles
    - Added a different 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@
             $routeProvider
                 .when("/notes/:id", { templateUrl: "note/note.html" })
                 .when("/notes", { templateUrl: "notes/notes.html" })
+                .when("/", { redirectTo: '/notes' })
                 .otherwise({ templateUrl: "404/404.html" });
         }
     }
