494d9119e31830a0a7cefd1478a915585a875bf1
James Honzatko <bejbej.uit@gmail.com>
2017-06-02 21:54:21 -0500

    Updated styles.
    - Notes view uses a narrower page width
    - Slightly increased the font of the filters select options

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,24 @@
 ﻿module app {
+    interface IRootScopeService extends ng.IRootScopeService {
+        pageSize: string;
+    }
+
     class Route {
     
         constructor($routeProvider: ng.route.IRouteProvider) {
             $routeProvider
                 .when("/notes/:id", { templateUrl: "note/note.html" })
-                .when("/notes", { templateUrl: "notes/notes.html" })
+                .when("/notes", <any>{ templateUrl: "notes/notes.html", pageSize: "sm" })
                 .when("/", { redirectTo: '/notes' })
                 .otherwise({ templateUrl: "404/404.html" });
         }
     }
 
     angular.module("app").config(Route);
+
+    angular.module("app").run(($rootScope: IRootScopeService) => {
+        $rootScope.$on("$routeChangeSuccess", (event, route) => {
+            $rootScope.pageSize = route.pageSize;
+        });
+    });
 }
